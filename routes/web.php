4b8020a7ba1c99c56c40eb6c239792345c60ce9d
Shobri24 <shobri2424@gmail.com>
2024-09-02 10:29:26 +0700

    JS2_Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241760092_Muhammad Shobri Al Mughdhor';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' .$postId. "Komentar ke-:" .$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama Saya ' . $name;
+});
+
+Route::get ('/hello', [WelcomeController::class,'hello']);
