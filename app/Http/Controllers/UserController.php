e9220998aa0ff0f381339de467db8b3d4d501db1
Shobri24 <shobri2424@gmail.com>
2024-09-23 10:18:33 +0700

    JS4_Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,35 +9,46 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-   public function index()
-{
-    /*$user = UserModel::firstOrCreate(
-        [
-            'username' => 'manager22',
-            'nama' => 'Manager Dua Dua',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ],
-        );*/
-
-        /*$user = UserModel::firstOrNew(
+    public function index()
+    {
+       /* $user = UserModel::firstOrNew(
             [
-                'username' => 'manager',
-                'nama' => 'Manager'
-            ],
-            );*/
+                'username' => 'manager55',
+                'nama' => 'Manager55',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ]);
 
-            $user = UserModel::firstOrNew(
+            $user->username = 'manager56';
+            $user->isDirty();
+            $user->isDirty('username');
+            $user->isDirty('nama');
+            $user->isDirty(['nama', 'username']);
+            
+            $user->isClean();
+            $user->isClean('username');
+            $user->isClean('nama');
+            $user->isClean(['nama', 'username']);
+
+            $user->isDirty();
+            $user->isClean();
+            dd($user->isDirty());
+            */
+
+            $user = UserModel::create(
                 [
-                    'username' => 'manager33',
-                    'nama' => 'Manager Tiga Tiga',
+                    'username' => 'manager11',
+                    'nama' => 'Manager11',
                     'password' => Hash::make('12345'),
-                    'level_id' => 2
-                ],
-                );
+                    'level_id' => 2,
+                ]);
+    
+                $user->username = 'manager12';
                 $user->save();
-    return view('user', ['data' => $user]);
-}
-
-
+                $user->wasChanged();
+                $user->wasChanged('username');
+                $user->wasChanged(['username', 'level_id']);
+                $user->wasChanged('nama');
+                dd($user->wasChanged(['nama', 'username']));
+        }
 }
