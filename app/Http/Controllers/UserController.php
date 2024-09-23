c342b98bcbc3006eddfcc8368bfd571f98d1a376
Shobri24 <shobri2424@gmail.com>
2024-09-23 08:13:02 +0700

    JS4_Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -10,14 +11,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        //$user = UserModel::find(1);
-        //$user = UserModel::firstWhere('level_id', 1);
-        /*$user = UserModel::findOr(1, ['username', 'nama'], function () {
-            abort(404);
-        });*/
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        //$user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
