00db8d31386d3d0c951afd2230fe940d406de1de
Shobri24 <shobri2424@gmail.com>
2024-10-14 15:46:42 +0700

    JS8_Praktikum 3

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -6,6 +6,7 @@ use App\Models\LevelModel;
 use App\Models\BarangModel;
 use App\Models\KategoriModel;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Yajra\DataTables\Facades\DataTables;
@@ -32,7 +33,7 @@ class BarangController extends Controller
     {
         $barang = BarangModel::select('barang_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'kategori_id')->with('kategori');
         $kategori_id = $request->input('filter_kategori');
-        
+
         if (!empty($kategori_id)) {
             $barang->where('kategori_id', $kategori_id);
         }
@@ -40,9 +41,9 @@ class BarangController extends Controller
         return DataTables::of($barang)
             ->addIndexColumn()
             ->addColumn('aksi', function ($barang) {
-                $btn = '<button onclick="modalAction(\''.url('/barang/'.$barang->barang_id.'/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button>';
-                $btn .= '<button onclick="modalAction(\''.url('/barang/'.$barang->barang_id.'/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button>';
-                $btn .= '<button onclick="modalAction(\''.url('/barang/'.$barang->barang_id.'/delete_ajax').'\')" class="btn btn-danger btn-sm">Hapus</button>';
+                $btn = '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button>';
+                $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button>';
+                $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button>';
                 return $btn;
             })
             ->rawColumns(['aksi'])
@@ -91,7 +92,7 @@ class BarangController extends Controller
     {
         $barang = BarangModel::find($id);
         $level = LevelModel::select('level_id', 'level_nama')->get();
-        
+
         return view('barang.edit_ajax', ['barang' => $barang, 'level' => $level]);
     }
 
@@ -100,7 +101,7 @@ class BarangController extends Controller
         if ($request->ajax() || $request->wantsJson()) {
             $rules = [
                 'kategori_id' => ['required', 'integer', 'exists:m_kategori,kategori_id'],
-                'barang_kode' => ['required', 'min:3', 'max:20', 'unique:m_barang,barang_kode,'.$id.',barang_id'],
+                'barang_kode' => ['required', 'min:3', 'max:20', 'unique:m_barang,barang_kode,' . $id . ',barang_id'],
                 'barang_nama' => ['required', 'string', 'max:100'],
                 'harga_beli' => ['required', 'numeric'],
                 'harga_jual' => ['required', 'numeric'],
@@ -117,7 +118,7 @@ class BarangController extends Controller
             }
 
             $check = BarangModel::find($id);
-            
+
             if ($check) {
                 $check->update($request->all());
 
@@ -146,7 +147,7 @@ class BarangController extends Controller
     {
         if ($request->ajax() || $request->wantsJson()) {
             $barang = BarangModel::find($id);
-            
+
             if ($barang) {
                 $barang->delete();
                 return response()->json([
@@ -229,66 +230,89 @@ class BarangController extends Controller
 
 
     public function export_excel()
-{
-    // Ambil data barang yang akan diekspor
-    $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
-        ->orderBy('kategori_id')
-        ->with('kategori')
-        ->get();
+    {
+        // Ambil data barang yang akan diekspor
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->with('kategori')
+            ->get();
 
-    // Load library PhpSpreadsheet
-    $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet(); // Ambil sheet yang aktif
+        // Load library PhpSpreadsheet
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet(); // Ambil sheet yang aktif
 
-    // Set header kolom
-    $sheet->setCellValue('A1', 'No');
-    $sheet->setCellValue('B1', 'Kode Barang');
-    $sheet->setCellValue('C1', 'Nama Barang');
-    $sheet->setCellValue('D1', 'Harga Beli');
-    $sheet->setCellValue('E1', 'Harga Jual');
-    $sheet->setCellValue('F1', 'Kategori');
-    $sheet->getStyle('A1:F1')->getFont()->setBold(true); // Bold header
+        // Set header kolom
+        $sheet->setCellValue('A1', 'No');
+        $sheet->setCellValue('B1', 'Kode Barang');
+        $sheet->setCellValue('C1', 'Nama Barang');
+        $sheet->setCellValue('D1', 'Harga Beli');
+        $sheet->setCellValue('E1', 'Harga Jual');
+        $sheet->setCellValue('F1', 'Kategori');
+        $sheet->getStyle('A1:F1')->getFont()->setBold(true); // Bold header
 
-    $no = 1; // Nomor data dimulai dari 1
-    $baris = 2; // Baris data dimulai dari baris ke 2
+        $no = 1; // Nomor data dimulai dari 1
+        $baris = 2; // Baris data dimulai dari baris ke 2
 
-    // Loop data barang dan masukkan ke dalam sheet
-    foreach ($barang as $key => $value) {
-        $sheet->setCellValue('A' . $baris, $no);
-        $sheet->setCellValue('B' . $baris, $value->barang_kode);
-        $sheet->setCellValue('C' . $baris, $value->barang_nama);
-        $sheet->setCellValue('D' . $baris, $value->harga_beli);
-        $sheet->setCellValue('E' . $baris, $value->harga_jual);
-        $sheet->setCellValue('F' . $baris, $value->kategori->kategori_nama); // Ambil nama kategori
-        $baris++;
-        $no++;
+        // Loop data barang dan masukkan ke dalam sheet
+        foreach ($barang as $key => $value) {
+            $sheet->setCellValue('A' . $baris, $no);
+            $sheet->setCellValue('B' . $baris, $value->barang_kode);
+            $sheet->setCellValue('C' . $baris, $value->barang_nama);
+            $sheet->setCellValue('D' . $baris, $value->harga_beli);
+            $sheet->setCellValue('E' . $baris, $value->harga_jual);
+            $sheet->setCellValue('F' . $baris, $value->kategori->kategori_nama); // Ambil nama kategori
+            $baris++;
+            $no++;
+        }
+
+        // Set auto size untuk kolom A sampai F
+        foreach (range('A', 'F') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+
+        // Set judul sheet
+        $sheet->setTitle('Data Barang');
+
+        // Membuat writer untuk menulis file Excel
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $filename = 'Data_Barang_' . date('Y-m-d_H-i-s') . '.xlsx';
+
+        // Set header untuk download file Excel
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=1'); // Bypass cache IE
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Expired date in the past
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // Always modified
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0
+
+        // Simpan file ke output
+        $writer->save('php://output');
+        exit;
     }
+    public function export_pdf()
+    {
+        // Ambil data barang yang akan diekspor
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->orderBy('barang_kode')
+            ->with('kategori')
+            ->get();
 
-    // Set auto size untuk kolom A sampai F
-    foreach (range('A', 'F') as $columnID) {
-        $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        // Menggunakan Barryvdh\DomPDF\Facade\Pdf untuk membuat PDF
+        $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+
+        // Set ukuran kertas dan orientasi
+        $pdf->setPaper('a4', 'portrait');
+
+        // Set opsi jika ada gambar dari URL, set true
+        $pdf->setOption("isRemoteEnabled", true);
+
+        // Render PDF
+        $pdf->render();
+
+        // Mengembalikan file PDF yang dihasilkan dalam bentuk stream
+        return $pdf->stream('Data_Barang_' . date('Y-m-d H:i:s') . '.pdf');
     }
-
-    // Set judul sheet
-    $sheet->setTitle('Data Barang');
-
-    // Membuat writer untuk menulis file Excel
-    $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
-    $filename = 'Data_Barang_' . date('Y-m-d_H-i-s') . '.xlsx';
-
-    // Set header untuk download file Excel
-    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    header('Content-Disposition: attachment;filename="' . $filename . '"');
-    header('Cache-Control: max-age=0');
-    header('Cache-Control: max-age=1'); // Bypass cache IE
-    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Expired date in the past
-    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // Always modified
-    header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-    header('Pragma: public'); // HTTP/1.0
-
-    // Simpan file ke output
-    $writer->save('php://output');
-    exit;
-}
-
 }
